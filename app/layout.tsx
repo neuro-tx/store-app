2b3c0782ffaacfcf2a07dcc5a32ddeb6e0f6a0e3
tarek fawzy <tf5234045@gmail.com>
2025-10-15 18:45:30 +0300

    setup arabic to be the main language

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,12 +23,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ar" dir="rtl">
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
