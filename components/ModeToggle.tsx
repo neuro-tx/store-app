180e2fd3652abea802cc0b7b9c7984356a21b36e
tarek fawzy <tf5234045@gmail.com>
2025-10-17 01:25:32 +0300

    fix : fix import in some compnoent issues

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Settings, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -18,21 +18,35 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" className="border" size="icon">
+        <Button
+          variant="ghost"
+          className="border"
+          size="icon"
+          aria-label="تبديل المظهر"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+
+      <DropdownMenuContent align="end" side="bottom">
         <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
+          <div className="flex items-center justify-end text-right w-full gap-2">
+            <span>الوضع النهاري</span>
+            <Sun />
+          </div>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
+          <div className="flex items-center justify-end text-right w-full gap-2">
+            <span>الوضع الليلي</span>
+            <Moon />
+          </div>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
+          <div className="flex items-center justify-end text-right w-full gap-2">
+            <span>حسب النظام</span>
+            <Settings />
+          </div>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
