f6d1fc922ebb760f895c9f8148da8a6f0bdfcba8
tarek fawzy <tf5234045@gmail.com>
2025-10-18 19:37:29 +0300

    feat : add category form

---
+++ app/admin/_components/CategoryForm.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Loader, Plus } from "lucide-react";
+import React, { useTransition } from "react";
+import { FormProvider, useForm } from "react-hook-form";
+import Uploader from "./Uploader";
+
+const CategoryForm = () => {
+  const [submiting, startSubmit] = useTransition();
+  const onSubmit = (data: any) => {
+    startSubmit(async () => {
+      console.log(data);
+    });
+  };
+
+  const methods = useForm();
+  const form = useForm({});
+  return (
+    <div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <Card>
+            <CardHeader className="border-b">
+              <CardTitle className="font-cairo font-semibold text-lg">
+                صورة الفئة
+              </CardTitle>
+              <CardDescription className="text-sm text-muted-foreground">
+                قم برفع صورة تعبّر عن الفئة لعرضها في واجهة المتجر.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <Uploader />
+            </CardContent>
+          </Card>
+
+          <Card>
+            <CardHeader className="border-b">
+              <CardTitle className="font-cairo font-semibold text-lg">
+                المعلومات الأساسية
+              </CardTitle>
+              <CardDescription className="text-sm text-muted-foreground">
+                أدخل التفاصيل الأساسية الخاصة بالفئة، مثل الاسم والوصف
+              </CardDescription>
+            </CardHeader>
+
+            <CardContent className="space-y-4 mt-4">
+              <FormField
+                name="name"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base font-semibold">
+                      اسم الفئة
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        placeholder="مثال: التوابل والأعشاب"
+                        className="text-sm"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                name="description"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-base font-semibold">
+                      وصف الفئة
+                    </FormLabel>
+                    <FormControl>
+                      <Textarea
+                        className="text-sm min-h-32"
+                        placeholder="اكتب وصفًا موجزًا للفئة يوضح نوع المنتجات التي تضمها..."
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </CardContent>
+          </Card>
+
+          <Button
+            type="submit"
+            disabled={submiting}
+            className="w-full md:w-fit bg-primary text-white hover:bg-primary/90 cursor-pointer"
+          >
+            {submiting ? (
+              <div className="flex items-center gap-2 text-base">
+                <Loader className="animate-spin" />
+                <span>جارٍ الإنشاء...</span>
+              </div>
+            ) : (
+              <div className="flex items-center gap-2">
+                <Plus />
+                <span>إنشاء فئة</span>
+              </div>
+            )}
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default CategoryForm;
--- app/admin/categories/page.tsx
+++ app/admin/categories/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { MoveLeft } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+import CategoryForm from "../_components/CategoryForm";
 
 const CategoriesAdmin = () => {
   return (
-    <div>Categories Admin Page</div>
-  )
-}
+    <div className="w-full min-h-svh overflow-x-hidden">
+      <div className="container mx-auto">
+        <div className="p-4 lg:px-6 py-3">
+          <div className="space-y-6 px-2">
+            <div className="flex items-center justify-between">
+              <h2 className="font-semibold text-xl font-cairo">
+                إدارة تصنيفات المنتجات
+              </h2>
 
-export default CategoriesAdmin
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Link
+                    href="/admin"
+                    className="px-2 py-1 bg-primary/20 rounded-xl"
+                  >
+                    <MoveLeft size={22} />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <span className="text-sm">رجوع</span>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+
+            <CategoryForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesAdmin;
