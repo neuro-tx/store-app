c8323137bf2d514d98f62d6501c0a20d5e808571
tarek fawzy <tf5234045@gmail.com>
2025-10-27 01:16:11 +0300

    feat: add auth ui

---
+++ app/auth/layout.tsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { ThemedToaster } from "@/components/ThemedToaster ";
+import React from "react";
+import "../globals.css";
+
+const layout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang="ar" dir="rtl" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div className="min-h-svh flex w-full items-center justify-center">
+            {children}
+          </div>
+          <ThemedToaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
---
+++ app/auth/page.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Loader2 } from "lucide-react";
+import React, { useState, useTransition } from "react";
+import { toast } from "sonner";
+import z from "zod";
+
+const loginSchema = z.object({
+  userKey: z
+    .string()
+    .min(5, { message: "يجب أن يحتوي مفتاح المستخدم على 5 أحرف على الأقل." })
+    .nonempty({ message: "حقل مفتاح المستخدم مطلوب." }),
+
+  password: z
+    .string()
+    .min(6, { message: "يجب أن تتكون كلمة المرور من 6 أحرف على الأقل." })
+    .nonempty({ message: "حقل كلمة المرور مطلوب." }),
+});
+
+const AuthPage = () => {
+  const [userKey, setUserKey] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isPending, transition] = useTransition();
+
+  const submit = () => {
+    transition(async () => {
+      const validate = loginSchema.safeParse({ userKey, password });
+
+      if (!validate.success) {
+        toast.error("حدث خطأ أثناء التحقق من البيانات.");
+        return;
+      }
+
+      try {
+        const { password: pwd, userKey: usKey } = validate.data;
+        console.log(usKey, pwd);
+      } catch (error) {
+        console.error("Login error:", error);
+        toast.error("حدث خطأ غير متوقع أثناء تسجيل الدخول.");
+      }
+    });
+  };
+  return (
+    <Card className="">
+      <CardHeader>
+        <CardTitle className="font-cairo text-xl font-bold">
+          تسجيل دخول المدير
+        </CardTitle>
+        <CardDescription className="text-muted-foreground text-sm leading-relaxed">
+          أدخل مفتاح المستخدم وكلمة المرور للوصول إلى لوحة التحكم.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-5">
+        <div className="space-y-2">
+          <Input
+            type="text"
+            placeholder="أدخل مفتاح المستخدم"
+            required
+            className="text-sm"
+            value={userKey}
+            onChange={(e) => setUserKey(e.target.value)}
+          />
+          <Input
+            type="password"
+            placeholder="أدخل كلمة المرور"
+            required
+            className="text-sm"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <Button
+          type="button"
+          className="w-full text-sm text-white cursor-pointer"
+          onClick={submit}
+          disabled={isPending}
+        >
+          {isPending ? (
+            <div className="flex items-center justify-center gap-2">
+              <Loader2 className="h-4 w-4 animate-spin" />
+              <span>جارِ تسجيل الدخول...</span>
+            </div>
+          ) : (
+            "تسجيل الدخول"
+          )}{" "}
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AuthPage;
